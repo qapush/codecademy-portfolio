7931d3603a6696dd67c9c4f10ac8c3bc2557630f
qapush <mihail.kapush@gmail.com>
2023-01-15 17:15:36 +0100

    Add menu

---
+++ assets/js/app.js
@@ -0,0 +1,7 @@
+// Mobile menu button
+
+const mobileMenuButton = document.querySelector('.hamburger');
+
+mobileMenuButton.addEventListener('click', () => {
+    mobileMenuButton.classList.toggle('is-active')
+})
