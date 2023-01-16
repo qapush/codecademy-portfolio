f20a08b035375c68e7b2ed4ac0f5949ad2cd9940
Mykhailo Kapush <m.kapush@mld.local>
2023-01-16 13:02:12 +0100

    Add lottie instead of portrait

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,3 +5,9 @@ const mobileMenuButton = document.querySelector('.hamburger');
 mobileMenuButton.addEventListener('click', () => {
     mobileMenuButton.classList.toggle('is-active')
 })
+
+
+// Center lottie animation
+const player = document.querySelector('.lottie');
+player.shadowRoot.querySelector('.main').style.margin = 'auto';
+player.shadowRoot.querySelector('.animation').style.margin = 'auto';
